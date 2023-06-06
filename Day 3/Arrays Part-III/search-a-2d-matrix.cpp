8dda0447478772260a2416ac04d5f0fe12f319ce
Arun Kumar Pahadia <80753977+pahadiaarun@users.noreply.github.com>
2023-06-06 13:05:35 +0530

    Update search-a-2d-matrix.cpp

--- Day 3/Arrays Part-III/search-a-2d-matrix.cpp
+++ Day 3/Arrays Part-III/search-a-2d-matrix.cpp
@@ -1,1 +1,15 @@
+// https://leetcode.com/problems/search-a-2d-matrix/
 
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size(),m=matrix[0].size();
+        int i=0,j=m-1;
+        while(i<n && j>=0){
+            if(matrix[i][j]==target) return 1;
+            else if(matrix[i][j]>target) j--;
+            else i++;
+        }
+        return 0;
+    }
+};
