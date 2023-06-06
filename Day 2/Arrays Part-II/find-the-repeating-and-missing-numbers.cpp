475bb9bfe8d28661d9262773ba641b91ae3bd564
Arun Kumar Pahadia <80753977+pahadiaarun@users.noreply.github.com>
2023-06-06 12:58:53 +0530

    Create find-the-repeating-and-missing-numbers.cpp

---
+++ Day 2/Arrays Part-II/find-the-repeating-and-missing-numbers.cpp
@@ -0,0 +1,15 @@
+// https://takeuforward.org/data-structure/find-the-repeating-and-missing-numbers/
+
+vector<int> Solution::repeatedNumber(const vector<int> &A) {
+    long long n=A.size();;
+    long long x=(n*(n+1))/2;
+    long long y=(n*(n+1)*(2*n+1))/6;
+    for(auto i:A){
+        x-=(long long)i;
+        y-=((long long)i*(long long)i);
+    }
+    int a=(x+(y/x))/2;
+    int b=a-x;
+    return {b,a};
+}
+
