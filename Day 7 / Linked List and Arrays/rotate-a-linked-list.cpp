a9812e90f185993354a01bdb201ceebb0efaa9a1
Arun Kumar Pahadia <80753977+pahadiaarun@users.noreply.github.com>
2023-06-08 03:27:35 +0530

    Update rotate-a-linked-list.cpp

--- Day 7 / Linked List and Arrays/rotate-a-linked-list.cpp
+++ Day 7 / Linked List and Arrays/rotate-a-linked-list.cpp
@@ -1,1 +1,32 @@
+// https://leetcode.com/problems/rotate-list/
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head) return head;
+        int n=1;
+        ListNode* p=head;
+        while(p->next){
+            n++;
+            p=p->next;
+        }
+        p->next=head;
+        k=k%n;
+        n=n-k-1;
+        ListNode* q=head;
+        while(n--) q=q->next;
+        head=q->next;
+        q->next=NULL;
+        return head;
+    }
+};
