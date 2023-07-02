dffa589779fa5de6b8101a3a9511187152222b96
Arun Kumar Pahadia <80753977+pahadiaarun@users.noreply.github.com>
2023-07-02 15:16:29 +0530

    Create mirror-tree.cpp

---
+++ Day19/Binary Tree Part-3/mirror-tree.cpp
@@ -0,0 +1,13 @@
+// https://practice.geeksforgeeks.org/problems/mirror-tree/1
+
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node* node) {
+        // code here
+        if(!node) return;
+        swap(node->left,node->right);
+        mirror(node->left);
+        mirror(node->right);
+    }
+};
