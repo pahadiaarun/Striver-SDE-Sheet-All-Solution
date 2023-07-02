8752b2342acf52a22b79ef502fce154db91f5df8
Arun Kumar Pahadia <80753977+pahadiaarun@users.noreply.github.com>
2023-07-02 15:25:58 +0530

    Create flatten-binary-tree-to-linked-list.cpp

---
+++ Day21/Binary Search Tree Part-2/flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+
+#include <bits/stdc++.h> 
+void inorder(BinaryTreeNode<int>* root, vector<int>&v){
+    if(root==NULL)return;
+    inorder(root->left,v);
+    v.push_back(root->data);
+    inorder(root->right,v);
+}
+BinaryTreeNode<int>* BTtoDLL(BinaryTreeNode<int>* root) {
+    vector<int>v;
+    inorder(root,v);
+    BinaryTreeNode<int>* head = new BinaryTreeNode<int>(-1);
+    BinaryTreeNode<int>* curr = head;
+    BinaryTreeNode<int>* prev = NULL;
+    for(auto&x:v){
+        BinaryTreeNode<int>* t = new BinaryTreeNode<int>(x);
+        curr->right = t;
+        curr->left = prev;
+        prev = curr;
+        curr = curr->right;
+    }
+    curr->right = NULL;
+    return head->right;
+}
+
