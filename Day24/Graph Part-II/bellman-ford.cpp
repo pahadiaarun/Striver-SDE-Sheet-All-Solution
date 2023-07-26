a4cf28429f3bb3ff3cc0292a9d99dfdc1d4a5d8c
Arun Kumar Pahadia <80753977+pahadiaarun@users.noreply.github.com>
2023-07-27 01:26:16 +0530

    Create bellman-ford.cpp

---
+++ Day24/Graph Part-II/bellman-ford.cpp
@@ -0,0 +1,78 @@
+// https://practice.geeksforgeeks.org/problems/distance-from-the-source-bellman-ford-algorithm/0?fbclid=IwAR2_lL0T84DnciLyzMTQuVTMBOi82nTWNLuXjUgahnrtBgkphKiYk6xcyJU
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    /*  Function to implement Bellman Ford
+    *   edges: vector of vectors which represents the graph
+    *   S: source vertex to start traversing graph with
+    *   V: number of vertices
+    */
+    vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+        // Code here
+        vector<int>dist(V,100000000);
+        dist[S]=0;
+        
+        for(int i=0;i<V-1;i++){
+            for(auto i:edges){
+                int u=i[0],v=i[1],w=i[2];
+                if(dist[u]+w<dist[v]){
+                    dist[v]=dist[u]+w;
+                }
+            }
+        }
+        vector<int>temp=dist;
+        for(auto i:edges){
+            int u=i[0],v=i[1],w=i[2];
+            if(temp[u]+w<temp[v]){
+                temp[v]=temp[u]+w;
+            }
+        }
+        if(temp!=dist) return {-1};
+        return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, m;
+        cin >> N >> m;
+        vector<vector<int>> edges;
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+
+        int src;
+        cin >> src;
+
+        Solution obj;
+        vector<int> res = obj.bellman_ford(N, edges, src);
+
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
