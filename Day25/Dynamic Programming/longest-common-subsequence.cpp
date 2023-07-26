a6e5b0b680da50695c7c4b4070dc7cf5d2d8c94b
Arun Kumar Pahadia <80753977+pahadiaarun@users.noreply.github.com>
2023-07-27 01:30:08 +0530

    Create longest-common-subsequence.cpp

---
+++ Day25/Dynamic Programming/longest-common-subsequence.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/longest-common-subsequence/
+
+#include <bits/stdc++.h> 
+int lcs(string s, string t)
+{
+  //Write your code here
+  int n=s.size(),m=t.size();
+  vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=m;j++){
+      if(s[i-1]==t[j-1]){
+        dp[i][j]=1+dp[i-1][j-1];
+      }
+      else{
+        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+      }
+    }
+  }
+  return dp[n][m];
+}
+
